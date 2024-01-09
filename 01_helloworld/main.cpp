60943737574dced7b7e01805f8ec4ce33a08c144
Vitaly Isaev <vitalyisaev2@gmail.com>
2024-01-10 01:56:35 +0300

    passing build number

--- 01_helloworld/main.cpp
+++ 01_helloworld/main.cpp
@@ -5,8 +5,7 @@
 
 int main() {
   std::stringstream ss;
-  ss << "build " << HelloWorld_VERSION_MAJOR << "." << HelloWorld_VERSION_MINOR
-     << "." << HelloWorld_VERSION_PATCH << std::endl;
+  ss << "build " << BUILD_NUMBER << std::endl;
   ss << "Hello, World!" << std::endl;
 
   std::cout << ss.str();
