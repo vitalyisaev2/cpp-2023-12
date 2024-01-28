a236bf2ac0c7d1e91115ef496bfb9d8e27b96960
Vitaly Isaev <vitalyisaev2@gmail.com>
2024-01-28 22:30:18 +0300

    HW 02 review fixes

--- 02_ip_filter/address.hpp
+++ 02_ip_filter/address.hpp
@@ -15,42 +15,16 @@ namespace NIPFilter {
         TAddress() = delete;
 
         TAddress(uint8_t p1, uint8_t p2, uint8_t p3, uint8_t p4)
-            : octets({p1, p2, p3, p4})
-                  {};
+            : octets({p1, p2, p3, p4}){};
 
         TAddress(const std::string& in);
 
+        friend std::ostream& operator<<(std::ostream& os, const TAddress& address);
+
+        uint8_t GetOctet(std::size_t i) const;
+        bool HasOctet(uint8_t value) const;
+        bool operator==(const TAddress& other) const;
         bool operator<(const TAddress& other) const;
-
-        friend std::ostream& operator<<(std::ostream& os, const TAddress& address)
-        {
-            for (std::size_t i = 0; i < 4; i++)
-            {
-                os << std::to_string(address.octets[i]);
-                if (i < 3)
-                    os << ".";
-            }
-            return os;
-        }
-
-        uint8_t GetOctet(std::size_t i) const {
-            if (i > 3)
-            {
-                std::stringstream ss;
-                ss << "Invalid octet index: " << std::to_string(i) << std::endl;
-                throw ss.str();
-            }
-
-            return octets[i];
-        }
-
-        bool HasOctet(uint8_t value) const {
-            return std::find(octets.cbegin(), octets.cend(), value) != std::end(octets);
-        };
-
-        bool operator==(const TAddress& rhs) const {
-            return this->octets == rhs.octets;
-        }
 
     private:
         std::array<uint8_t, 4> octets;
@@ -58,6 +32,6 @@ namespace NIPFilter {
         void GetNextByte(const std::string& in, uint8_t& out, std::size_t& start);
     };
 
-}
+} //namespace NIPFilter
 
 #endif
