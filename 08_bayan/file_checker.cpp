2c8ace512cb62f2ce5445e4566c46b728ad455f1
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-04-02 01:01:28 +0300

    Block Size

--- 08_bayan/file_checker.cpp
+++ 08_bayan/file_checker.cpp
@@ -1,3 +1,4 @@
+#include <boost/crc.hpp>
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 
@@ -8,7 +9,17 @@ namespace NBayan {
         const boost::interprocess::mode_t mode = boost::interprocess::read_only;
         boost::interprocess::file_mapping fm(file.Name.c_str(), mode);
         boost::interprocess::mapped_region region(fm, mode, 0, 0);
-        const char* begin = static_cast<const char*>(region.get_address());
+
+        const char* blockStart = static_cast<const char*>(region.get_address());
+        // shift to the first byte of requested block 
+        blockStart += blockId * BlockSize;
+
+        const char* blockEnd = blockStart + BlockSize;
+        const char* regionEnd = static_cast<const char*>(region.get_address()) + region.get_size();
+        if (blockEnd >= regionEnd) {
+            blockEnd = regionEnd;
+            // TODO: need to fill up to block size
+        } 
 
         return 0;
     }
