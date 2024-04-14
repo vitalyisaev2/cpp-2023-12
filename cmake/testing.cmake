b16f2a70c8aef883ed6b06c2b12fdd43c6ec1bb2
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-04-14 23:33:31 +0300

    Cleanup testing.cmake

--- cmake/testing.cmake
+++ cmake/testing.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.20)
 
-macro(run_tests library)
+macro(run_tests libraries)
     include(CTest)
     include(GoogleTest)
     find_package(GTest)
@@ -12,7 +12,7 @@ macro(run_tests library)
     add_executable("${TESTS_BINARY_NAME}" tests.cpp)
     # target_compile_options("${TESTS_BINARY_NAME}" PRIVATE -Wall -Wextra -pedantic -Werror)
     target_include_directories("${TESTS_BINARY_NAME}" PUBLIC "${PROJECT_BINARY_DIR}")
-    target_link_libraries("${TESTS_BINARY_NAME}" GTest::gtest_main "${library}" Boost::program_options Boost::system Boost::filesystem)
+    target_link_libraries("${TESTS_BINARY_NAME}" GTest::gtest_main "${libraries}")
     set_target_properties("${TESTS_BINARY_NAME}" PROPERTIES
         CXX_STANDARD 20
         CXX_STANDARD_REQUIRED ON
