fc11997c18e31c4ba9938bd2f9e5cedac8f8e432
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-05-21 00:02:56 +0300

    Extended checks

--- 10_bulk_server/check.go
+++ 10_bulk_server/check.go
@@ -8,14 +8,8 @@ import (
 	"sync"
 )
 
-const connections = 100
-
-func main() {
-	tcpServer, err := net.ResolveTCPAddr("tcp", "localhost:10000")
-	if err != nil {
-		fmt.Println("Resolve failed: ", err)
-		os.Exit(1)
-	}
+func check1(tcpServer *net.TCPAddr) {
+	const connections = 10
 
 	var wg sync.WaitGroup
 
@@ -44,9 +38,58 @@ func main() {
 				fmt.Println("Write failed: ", err)
 				os.Exit(1)
 			}
-
 		}(i)
 	}
 
 	wg.Wait()
+}
+
+func check2(tcpServer *net.TCPAddr) {
+
+	data1, err := os.ReadFile("./data1.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	data2, err := os.ReadFile("./data2.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	inputs := [][]byte{data1, data2}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < len(inputs); i++ {
+		wg.Add(1)
+		go func(input []byte) {
+			defer wg.Done()
+
+			conn, err := net.DialTCP("tcp", nil, tcpServer)
+			if err != nil {
+				fmt.Println("Dial failed: ", err)
+				os.Exit(1)
+			}
+
+			defer conn.Close()
+
+			if _, err := conn.Write(input); err != nil {
+				fmt.Println("Write failed: ", err)
+				os.Exit(1)
+			}
+		}(inputs[i])
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	tcpServer, err := net.ResolveTCPAddr("tcp", "localhost:10000")
+	if err != nil {
+		fmt.Println("Resolve failed: ", err)
+		os.Exit(1)
+	}
+
+	check1(tcpServer)
+	// check2(tcpServer)
 }
