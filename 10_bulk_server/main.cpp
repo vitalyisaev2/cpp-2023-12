7b1e95fae29dfb86476d6c87d85083d372263129
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-05-20 23:43:42 +0300

    Server is asynchronous and multithreaded right now

--- 10_bulk_server/main.cpp
+++ 10_bulk_server/main.cpp
@@ -9,6 +9,7 @@
 //
 
 #include "server.hpp"
+#include <boost/asio/io_context.hpp>
 
 int main(int argc, char* argv[]) {
     try {
@@ -20,10 +21,17 @@ int main(int argc, char* argv[]) {
         std::size_t port = std::atoi(argv[1]);
         std::size_t bulkSize = std::atoi(argv[2]);
 
-        boost::asio::io_service ioService;
-        TServer server(ioService, port, bulkSize);
+        boost::asio::io_context ioContext;
+        TServer server(ioContext, port, bulkSize);
 
-        // ioService.run();
+        std::vector<std::thread> threads;
+        for (std::size_t i = 0; i < std::thread::hardware_concurrency(); ++i) {
+            threads.emplace_back([&ioContext]() { ioContext.run(); });
+        }
+
+        for (auto& t : threads) {
+            t.join();
+        }
     } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
