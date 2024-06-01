9ae2ccaefe433376998f7a656cb858bc70d10f28
Vitaly Isaev <vitalyisaev2@gmail.com>
2024-06-01 21:28:45 +0300

    Not operational

--- 11_join_server/database.cpp
+++ 11_join_server/database.cpp
@@ -8,61 +8,71 @@
 namespace NDatabase {
 
     TResultQueue::TPtr MakeResultQueue() {
-        return MakeThreadSafeQueue<std::optional<TRowData>>();
+        return MakeThreadSafeQueue<TResult>();
     };
 
     void TDatabase::Insert(const std::string& tableName, TRowData&& rowData) {
-        {
-            // check if table with the requested name exists
-            std::shared_lock lock{Mutex_};
-            auto it = Tables_.find(tableName);
-            if (it != Tables_.end()) {
-                it->second->InsertRow(++TxCounter_, std::move(rowData));
-                return;
-            }
-        }
+        ThreadPool_->Enqueue(
+            [self = shared_from_this(), tableName = tableName, rowData = std::move(rowData)](std::size_t) {
+                {
+                    // check if table with the requested name exists
+                    std::shared_lock lock{self->Mutex_};
+                    auto it = self->Tables_.find(tableName);
+                    if (it != self->Tables_.end()) {
+                        auto txId = ++self->TxCounter_;
+                        it->second->InsertRow(txId, std::move(rowData));
+                        return;
+                    }
+                }
 
-        {
-            // re-check and create table if neccessary
-            std::unique_lock lock{Mutex_};
+                {
+                    // re-check and create table if necessary
+                    std::unique_lock lock{self->Mutex_};
+                    auto txId = ++self->TxCounter_;
 
-            auto it = Tables_.find(tableName);
-            if (it != Tables_.end()) {
-                it->second->InsertRow(++TxCounter_, std::move(rowData));
-                return;
-            }
+                    auto it = self->Tables_.find(tableName);
+                    if (it != self->Tables_.end()) {
+                        it->second->InsertRow(txId, std::move(rowData));
+                        return;
+                    }
 
-            auto table = MakeTable();
-            table->InsertRow(++TxCounter_, std::move(rowData));
+                    auto table = MakeTable();
+                    table->InsertRow(txId, std::move(rowData));
 
-            Tables_.emplace(tableName, std::move(table));
-        }
+                    self->Tables_.emplace(tableName, std::move(table));
+                }
+            });
     }
 
     void TDatabase::Select(const std::string& tableName, TResultQueue::TPtr queue) {
-        std::shared_lock lock{Mutex_};
-        auto it = Tables_.find(tableName);
-        if (it == Tables_.cend()) {
-            std::stringstream ss;
-            ss << "Table with name '" << tableName << "' does not exist";
-            throw ss.str();
-        }
+        ThreadPool_->Enqueue([self = shared_from_this(), tableName = tableName, queue = std::move(queue)](std::size_t) {
+            std::shared_lock lock{self->Mutex_};
+            auto it = self->Tables_.find(tableName);
+            if (it == self->Tables_.cend()) {
+                std::stringstream ss;
+                ss << "Table with name '" << tableName << "' does not exist";
+                throw ss.str();
+            }
 
-        it->second->Iterate(
-            ++TxCounter_, [&queue](TRowId rowId, std::optional<TRowData> rowData) { 
-                queue->Push(std::move(rowData)); });
+            auto txId = ++self->TxCounter_;
+            it->second->Iterate(
+                txId, [&queue](TRowId rowId, std::optional<TRowData> rowData) { queue->Push(std::move(rowData)); });
+        });
     }
 
     void TDatabase::Truncate(const std::string& tableName) {
-        std::shared_lock lock{Mutex_};
-        auto it = Tables_.find(tableName);
-        if (it == Tables_.cend()) {
-            std::stringstream ss;
-            ss << "Table with name '" << tableName << "' does not exist";
-            throw ss.str();
-        }
+        ThreadPool_->Enqueue([self = shared_from_this(), tableName = tableName](std::size_t) {
+            std::shared_lock lock{self->Mutex_};
+            auto it = self->Tables_.find(tableName);
+            if (it == self->Tables_.cend()) {
+                std::stringstream ss;
+                ss << "Table with name '" << tableName << "' does not exist";
+                throw ss.str();
+            }
 
-        it->second->Truncate(++TxCounter_);
+            auto txId = ++self->TxCounter_;
+            it->second->Truncate(txId);
+        });
     }
 
 } //namespace NDatabase
--- 11_join_server/database.hpp
+++ 11_join_server/database.hpp
@@ -4,15 +4,34 @@
 #include <shared_mutex>
 
 #include "table.hpp"
+#include "thread_pool.hpp"
 #include "thread_safe_queue.hpp"
 
 namespace NDatabase {
-    using TResultQueue = TThreadSafeQueue<std::optional<TRowData>>;
+    // TEndOfTable is a special type marking the end of table.
+    struct TEndOfTable {}; 
 
+    // TResult represents an elementary item of table reading process.
+    // It can contain either a table row, or a special message marking the end of table reading.
+    using TResult = std::variant<std::optional<TRowData>, TEndOfTable>;
+
+    // TResultQueue is used by TCP server to obtain the response data from the database
+    using TResultQueue = TThreadSafeQueue<TResult>;
     TResultQueue::TPtr MakeResultQueue();
 
-    class TDatabase {
+    // TDatabase manages the dataset and replies to the requests of a server.
+    class TDatabase: public std::enable_shared_from_this<TDatabase> {
     public:
+        TDatabase() = delete;
+
+        using TPtr = std::shared_ptr<TDatabase>;
+            static TPtr Create();
+
+    Cyclic(const Cyclic&) = delete;
+    Cyclic(Cyclic&&) = delete;
+    Cyclic& operator=(const Cyclic&) = delete;
+    Cyclic& operator=(Cyclic&&) = delete;
+
         void Insert(const std::string& tableName, TRowData&& rowData);
         void Select(const std::string& tableName, TResultQueue::TPtr queue);
         void Truncate(const std::string& tableName);
@@ -21,8 +40,11 @@ namespace NDatabase {
                                  TResultQueue::TPtr queue);
 
     private:
-        std::unordered_map<std::string, TTable::TPtr> Tables_;
-        std::shared_mutex Mutex_; // synchronizes access to tables maps
-        std::atomic<TTxId> TxCounter_;
+        std::unordered_map<std::string, TTable::TPtr> Tables_; // set of tables containing the data
+        std::shared_mutex Mutex_; // synchronizes access to tables map
+
+        std::atomic<TTxId> TxCounter_; // provides unique ids for transactions
+
+        TThreadPool<void>::TPtr ThreadPool_; // handles tasks in parallel threads
     };
 } //namespace NDatabase
