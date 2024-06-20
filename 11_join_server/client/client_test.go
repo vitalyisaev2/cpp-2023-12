ebeb403e14d2bad1093dbc28df16fcd862a7630c
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-06-21 01:01:01 +0300

    Test client pt. 2

--- 11_join_server/client/client_test.go
+++ 11_join_server/client/client_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
@@ -14,11 +15,16 @@ import (
 )
 
 func extractErrorFromResponse(response []byte) error {
-	if bytes.Equal(response, []byte("OK\n")) {
+	if bytes.Equal(response, []byte("OK")) {
 		return nil
 	}
 
-	return errors.New(string(response[:len(response)-2]))
+	return errors.New(string(response))
+}
+
+type record struct {
+	id    int
+	value string
 }
 
 type client struct {
@@ -26,30 +32,58 @@ type client struct {
 	mutex sync.Mutex
 }
 
-func (c *client) insert(tableName string, id int, value string) error {
+func (c *client) doInsert(tableName string, rec *record) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
-	request := fmt.Sprintf("INSERT %s %d %s\n", tableName, id, value)
+	request := fmt.Sprintf("INSERT %s %d %s\n", tableName, rec.id, rec.value)
 
 	log.Println("Sending request", request)
 
 	n, err := c.conn.Write([]byte(request))
 	if err != nil {
-		fmt.Println("Write failed: ", err)
-		return fmt.Errorf("write request: %w", err)
+		return fmt.Errorf("conn write: %w", err)
 	}
 
 	log.Println("Request sent", n)
 
 	response, err := io.ReadAll(c.conn)
 	if err != nil {
-		return fmt.Errorf("read response: %w", err)
+		return fmt.Errorf("conn read: %w", err)
 	}
 
 	log.Println("Received response", string(response))
 
 	return extractErrorFromResponse(response)
+}
+
+func (c *client) doSelect(tableName string) ([]*record, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	request := fmt.Sprintf("SELECT %s\n", tableName)
+
+	log.Println("Sending request", request)
+
+	n, err := c.conn.Write([]byte(request))
+	if err != nil {
+		fmt.Println("Write failed: ", err)
+		return nil, fmt.Errorf("write request: %w", err)
+	}
+
+	log.Println("Request sent", n)
+
+	scanner := bufio.NewScanner(c.conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		log.Println("Received response", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner err: %w", err)
+	}
+
+	return nil, nil
 }
 
 func (c *client) close() {
@@ -70,11 +104,13 @@ func newClient(endpoint string) (*client, error) {
 	return &client{conn: conn}, nil
 }
 
-func TestScenario(t *testing.T) {
+func TestMultilineTable(t *testing.T) {
 	cl, err := newClient("localhost:10000")
 	require.NoError(t, err)
 
 	defer cl.close()
 
-	cl.insert("A", 0, "lean")
+	require.NoError(t, cl.doInsert("A", &record{id: 0, value: "lean"}))
+	require.NoError(t, cl.doInsert("A", &record{id: 1, value: "understand"}))
+	cl.doSelect("A")
 }
